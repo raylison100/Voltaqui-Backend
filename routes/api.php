e92c9c50215c57ccb9fd61d9f13c8c59c1d6cf62
Raylison <raylison100@gmail.com>
2019-10-29 08:20:08 -0300

    Routes creation

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return redirect('/');
+});
+Route::group(['prefix' => 'voltaqui'], function () {
+    //  Gerenciamento de login
+    Route::post('oauth/token',                  '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken')->middleware('check-email-verification','insert-scope');//Login de usuario;
+    Route::get('activate/{token}',              'Auth\AuthController@enableSignUp');//Ativação de usuario via token em email.
+    // Gerenciamento de password
+    Route::group(['prefix' => 'password'], function () {
+        Route::post('email',        'Auth\PasswordResetController@create');// Solicitação de reset de senha passando email.
+        Route::get('find/{token}',  'Auth\PasswordResetController@find');// Validação de token para reset de senha.
+        Route::post('reset',        'Auth\PasswordResetController@reset');// Recebendo dados para auteração de senha.
+    });
+    // New User
+    Route::post('users',                    'UsersController@store');// Create a user.
+    // Routes Autenticadas
+    Route::group(['middleware' => ['auth:api','scope:COMMON']], function () {
+        // Authenticated
+        Route::get('auth',                      'Auth\AuthController@getUserAuthenticated');// Recupera usuario logado.
+        Route::delete('oauth/tokens',           'Auth\AuthController@destroyToken');//  Destroi token de acesso.
+        // User
+        Route::get('users',                     'UsersController@index');// Return all users.
+        Route::get('users/{id}',                'UsersController@show');// Return a users.
+        Route::put('users/{id}',                'UsersController@update');// Update a user.
+        Route::delete('users/{id}',             'UsersController@destroy');// Delete a user.
+
+    });
 });
